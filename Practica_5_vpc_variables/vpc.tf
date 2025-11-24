bd148eee16b823fa65529796896a062c3c816306
David Julian Molano Peralta <jmolano10@icloud.com>
2025-11-23 21:54:26 -0500

    Practica 5

--- Practica_5_vpc_variables/vpc.tf
+++ Practica_5_vpc_variables/vpc.tf
@@ -14,11 +14,3 @@ resource "aws_vpc" "vpc_djmp_ohio" {
   }
   provider = aws.ohio
 }
-
-variable "virginia_cidr" {
-  default = "10.10.0.0/16"
-}
-
-variable "ohio_cidr" {
-  default = "10.20.0.0/16"
-}
